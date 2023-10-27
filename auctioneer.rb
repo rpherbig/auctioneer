e4242fdcad67dc3d23f37708a5a41360747a5d84
Robert Herbig <sheltim@gmail.com>
2023-10-27 18:10:24 -0400

    Let's call this RC1

--- auctioneer.rb
+++ auctioneer.rb
@@ -3,26 +3,60 @@ require 'discordrb'
 ONE = "\u0031\uFE0F\u20E3"
 TWO = "\u0032\uFE0F\u20E3"
 THREE = "\u0033\uFE0F\u20E3"
+REACTION_TO_COUNT = {
+  ONE => 1,
+  TWO => 2,
+  THREE => 3,
+}
 BOT_ID = 1167205208909160488
 # S, L, K
-admins = [85187136659128320, 256665150180818946, 673546923051057183]
+ADMINS = [85187136659128320, 256665150180818946, 673546923051057183]
+@AUCTION_ITEMS = {
+  '600 species chest' => 8,
+  '300 species chest' => 16,
+  '100 species chest' => 80,
+  '30 alchemy case' => 16,
+  '20 coatings' => 16,
+  '60 research briefcase' => 16,
+  '100 organ chest' => 16,
+  '100 evolution chest' => 16,
+}
+
+@message_ids = {}
 
 bot = Discordrb::Commands::CommandBot.new token: IO.readlines("token.txt", chomp: true).first, prefix: '!'
 
 puts "This bot's invite URL is #{bot.invite_url}."
 puts 'Click on it to invite it to your server.'
 
-@auction_items = {
-  'Item A' => 12,
-  'Item B' => 8,
-}
-@message_ids = []
+def format_auction_item(item_name, remaining_quantity, max_quantity, user_bids)
+  bid_string = user_bids.length > 0 ? user_bids.join(", ") : "No bidders"
+  "#{item_name} (#{remaining_quantity}/#{max_quantity} left): #{bid_string}"
+end
 
 def recalculate_reactions(reaction_event)
-  return unless @message_ids.include?(reaction_event.message.id)
+  return unless @message_ids.keys.include?(reaction_event.message.id)
 
-  puts reaction_event.message.all_reaction_users()
-  # Filter out Bot ID
+  reactions = reaction_event.message.all_reaction_users()
+  new_quantity = 0
+  user_strings = []
+
+  [ONE, TWO, THREE].each do |r|
+    users = reactions[r].reject { |u| u.id == BOT_ID }
+    users.each do |u|
+      count = REACTION_TO_COUNT[r]
+      new_quantity += count
+      user_string = count == 1 ? u.username : "#{u.username} x#{count}"
+      user_strings.push(user_string)
+    end
+  end
+
+  item_name = @message_ids[reaction_event.message.id]
+  max_quantity = @AUCTION_ITEMS[item_name]
+  remaining = max_quantity - new_quantity
+  new_message = format_auction_item(item_name, remaining, max_quantity, user_strings)
+
+  reaction_event.message.edit(new_message)
 end
 
 #in: "#war-auction"
@@ -40,24 +74,28 @@ def add_reactions(message)
 end
 
 bot.command(:start, help_available: false) do |event|
-  break unless admins.include?(event.user.id)
+  break unless ADMINS.include?(event.user.id)
   puts 'Received start command'
 
-  bot.send_message(event.channel.id, 'Starting a new auction, please wait to bid!')
+  time = Time.new
+  date_string = time.strftime("%m/%d/%Y")
+  bot.send_message(event.channel.id, ":tada: Starting a new auction for #{date_string}, please wait to bid! :tada:")
 
-  @auction_items.each do |name, quantity|
-    e = bot.send_message(event.channel.id, "#{name} (0/#{quantity}): No bidders")
+  @AUCTION_ITEMS.each do |name, quantity|
+    message = format_auction_item(name, quantity, quantity, [])
+    e = bot.send_message(event.channel.id, message)
     add_reactions(e.message)
-    @message_ids.push(e.message.id)
+    @message_ids[e.message.id] = name
   end
 
+  bot.send_message(event.channel.id, 'Note: I am rate limited, so changes may take a minute to show up.')
   bot.send_message(event.channel.id, 'The auction is ready, please feel free to bid!')
 
   nil
 end
 
 bot.command(:stop, help_available: false) do |event|
-  break unless admins.include?(event.user.id)
+  break unless ADMINS.include?(event.user.id)
   puts 'Received stop command'
 
   bot.send_message(event.channel.id, 'Stopping the auction!')
@@ -68,7 +106,7 @@ bot.command(:stop, help_available: false) do |event|
 end
 
 bot.command(:exit, help_available: false) do |event|
-  break unless admins.include?(event.user.id)
+  break unless ADMINS.include?(event.user.id)
   puts 'Received exit command'
 
   bot.send_message(event.channel.id, 'Auctioneer is shutting down')
