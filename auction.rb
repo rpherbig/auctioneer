b68775262a96bd15f0bef28d1812dd7495542a2b
Robert Herbig <sheltim@gmail.com>
2023-11-07 22:49:16 -0500

    Split bot logic (dispatch) from auction logic (new file)
    This will make it easier to add new bot features in the future
    
    Fixes #8

---
+++ auction.rb
@@ -0,0 +1,116 @@
+# frozen_string_literal: true
+
+ONE = "\u0031\uFE0F\u20E3"
+TWO = "\u0032\uFE0F\u20E3"
+THREE = "\u0033\uFE0F\u20E3"
+REACTIONS = [ONE, TWO, THREE].freeze
+REACTION_TO_COUNT = {
+  ONE => 1,
+  TWO => 2,
+  THREE => 3,
+}.freeze
+BOT_ID = 1167205208909160488
+AUCTION_ITEMS = {
+  '600 species chest' => 8,
+  '300 species chest' => 16,
+  '100 species chest' => 80,
+  '30 alchemy case' => 16,
+  '20 coatings' => 16,
+  '60 research briefcase' => 16,
+  '100 organ chest' => 16,
+  '100 evolution chest' => 16,
+}.freeze
+
+class Auction
+  def initialize(bot, channelId)
+    @message_to_item = {}
+    @message_to_reactions = {}
+    @bot = bot
+    @channelId = channelId
+  end
+
+  def send(text)
+    @bot.send_message(@channelId, text)
+  end
+
+  def format_auction_item(item_name, remaining_quantity, max_quantity, user_bids)
+    bid_string = user_bids.any? ? user_bids.join(', ') : 'No bidders'
+    "#{item_name} (#{remaining_quantity}/#{max_quantity} left): #{bid_string}"
+  end
+
+  def add_reactions(message)
+    REACTIONS.each { |r| message.create_reaction(r) }
+  end
+
+  def start
+    time = Time.new
+    date_string = time.strftime('%m/%d/%Y')
+    send(":tada: Starting a new auction for #{date_string}, please wait a moment! :tada:")
+
+    AUCTION_ITEMS.each do |name, quantity|
+      message = format_auction_item(name, quantity, quantity, [])
+      e = send(message)
+      add_reactions(e.message)
+      @message_to_item[e.message] = name
+      @message_to_reactions[e.message] = {}
+    end
+
+    send('To claim something, react to its message with the quantity you want. For example, :two: means two of that item.
+Note: I am rate limited, so changes may take a minute to show up.
+:tada: The auction is ready! :tada:')
+  end
+
+  def auction_message?(message)
+    @message_to_item.keys.include?(message)
+  end
+
+  def recalculate_reactions(message)
+    reactions = message.all_reaction_users
+    @message_to_reactions[message] = reactions
+    new_quantity = 0
+    user_strings = []
+    users_seen = []
+
+    REACTIONS.each do |r|
+      users = reactions[r].reject { |u| u.id == BOT_ID }
+      users_seen.concat(users)
+      users.each do |u|
+        count = REACTION_TO_COUNT[r]
+        new_quantity += count
+        user_string = count == 1 ? u.display_name : "#{u.display_name} x#{count}"
+        user_strings.push(user_string)
+      end
+    end
+
+    item_name = @message_to_item[message]
+    max_quantity = AUCTION_ITEMS[item_name]
+    remaining = max_quantity - new_quantity
+    new_message = format_auction_item(item_name, remaining, max_quantity, user_strings)
+
+    send(":x: Item \"#{item_name}\" has too many bids. :x:") if remaining.negative?
+
+    duplicate_users = users_seen
+                      .select { |u| users_seen.count(u) > 1 }
+                      .uniq
+                      .map(&:mention)
+                      .join(' ')
+    unless duplicate_users.empty?
+      send(":x: Attention #{duplicate_users}: you have multiple bids on item \"#{item_name}\". Please only select one reaction per item. :x:")
+    end
+
+    overbid_users = @message_to_reactions # {message -> {reaction->[user]}}
+                    .values # [{reaction->[user]}]
+                    .map { |hash| hash.transform_keys { |key| REACTION_TO_COUNT[key] } } # [{count->[user]}]
+                    .each_with_object(Hash.new(0)) { |hash, accum| hash.each { |count, user_array| user_array.each { |user| accum[user] += count } } } # {user->count}
+                    .delete_if { |user, _count| user.id == BOT_ID } # {user->count}
+                    .delete_if { |_user, count| count <= 3 } # {user->count}
+                    .keys # [user]
+                    .map(&:mention) # [user]
+                    .join(' ') # string
+    unless overbid_users.empty?
+      send(":x: Attention #{overbid_users}: you have more than 3 bids across all items. Please only bid on up to 3 items. :x:")
+    end
+
+    message.edit(new_message)
+  end
+end
--- auctioneer.rb
+++ auctioneer.rb
@@ -1,35 +1,14 @@
 # frozen_string_literal: true
 
 require 'discordrb'
+require './auction.rb'
 
-ONE = "\u0031\uFE0F\u20E3"
-TWO = "\u0032\uFE0F\u20E3"
-THREE = "\u0033\uFE0F\u20E3"
-REACTIONS = [ONE, TWO, THREE].freeze
-REACTION_TO_COUNT = {
-  ONE => 1,
-  TWO => 2,
-  THREE => 3,
-}.freeze
-BOT_ID = 1167205208909160488
 # S, L, K
 ADMINS = [85187136659128320, 256665150180818946, 673546923051057183].freeze
-AUCTION_ITEMS = {
-  '600 species chest' => 8,
-  '300 species chest' => 16,
-  '100 species chest' => 80,
-  '30 alchemy case' => 16,
-  '20 coatings' => 16,
-  '60 research briefcase' => 16,
-  '100 organ chest' => 16,
-  '100 evolution chest' => 16,
-}.freeze
 
 class Auctioneer
   def initialize
     Discordrb::LOGGER.streams << File.open('log.txt', 'a')
-    @message_to_item = {}
-    @message_to_reactions = {}
 
     @bot = Discordrb::Commands::CommandBot.new token: File.readlines('token.txt', chomp: true).first, prefix: '!'
     @bot.command(:start, help_available: false) { |event| start(event) }
@@ -38,6 +17,8 @@ class Auctioneer
     @bot.reaction_add { |reaction_event| recalculate_reactions('add', reaction_event.message) }
     @bot.reaction_remove { |reaction_event| recalculate_reactions('remove', reaction_event.message) }
     at_exit { @bot.stop }
+
+    @auction = nil
 
     log('Bot started up')
   end
@@ -50,17 +31,8 @@ class Auctioneer
     Discordrb::LOGGER.info(text)
   end
 
-  def log_request(name, user)
-    log("Received '#{name}' request: #{user.display_name}, #{user.id}")
-  end
-
-  def log_reaction(type, message)
-    log("Type: '#{type}', Message ID: '#{message.id}', Reactions: #{message.all_reaction_users}")
-  end
-
-  def format_auction_item(item_name, remaining_quantity, max_quantity, user_bids)
-    bid_string = user_bids.any? ? user_bids.join(', ') : 'No bidders'
-    "#{item_name} (#{remaining_quantity}/#{max_quantity} left): #{bid_string}"
+  def log_request(type, user)
+    log("Received '#{type}' request: #{user.display_name}, #{user.id}")
   end
 
   def send(event, text)
@@ -68,61 +40,11 @@ class Auctioneer
   end
 
   def recalculate_reactions(type, message)
-    return unless @message_to_item.keys.include?(message)
+    return unless @auction && @auction.auction_message?(message)
 
-    # reactions?
+    log("Type: '#{type}', Message ID: '#{message.id}', Reactions: #{message.all_reaction_users}")
 
-    log_reaction(type, message)
-
-    reactions = message.all_reaction_users
-    @message_to_reactions[message] = reactions
-    new_quantity = 0
-    user_strings = []
-    users_seen = []
-
-    REACTIONS.each do |r|
-      users = reactions[r].reject { |u| u.id == BOT_ID }
-      users_seen.concat(users)
-      users.each do |u|
-        count = REACTION_TO_COUNT[r]
-        new_quantity += count
-        user_string = count == 1 ? u.display_name : "#{u.display_name} x#{count}"
-        user_strings.push(user_string)
-      end
-    end
-
-    item_name = @message_to_item[message]
-    max_quantity = AUCTION_ITEMS[item_name]
-    remaining = max_quantity - new_quantity
-    new_message = format_auction_item(item_name, remaining, max_quantity, user_strings)
-
-    send(message, ":x: Item \"#{item_name}\" has too many bids. :x:") if remaining.negative?
-
-    duplicate_users = users_seen
-                      .select { |u| users_seen.count(u) > 1 }
-                      .uniq
-                      .map(&:mention)
-                      .join(' ')
-    unless duplicate_users.empty?
-      send(message,
-           ":x: Attention #{duplicate_users}: you have multiple bids on item \"#{item_name}\". Please only select one reaction per item. :x:")
-    end
-
-    overbid_users = @message_to_reactions # {message -> {reaction->[user]}}
-                    .values # [{reaction->[user]}]
-                    .map { |hash| hash.transform_keys { |key| REACTION_TO_COUNT[key] } } # [{count->[user]}]
-                    .each_with_object(Hash.new(0)) { |hash, accum| hash.each { |count, user_array| user_array.each { |user| accum[user] += count } } } # {user->count}
-                    .delete_if { |user, _count| user.id == BOT_ID } # {user->count}
-                    .delete_if { |_user, count| count <= 3 } # {user->count}
-                    .keys # [user]
-                    .map(&:mention) # [user]
-                    .join(' ') # string
-    unless overbid_users.empty?
-      send(message,
-           ":x: Attention #{overbid_users}: you have more than 3 bids across all items. Please only bid on up to 3 items. :x:")
-    end
-
-    message.edit(new_message)
+    @auction.recalculate_reactions(message)
   end
 
   def add_reactions(message)
@@ -134,30 +56,8 @@ class Auctioneer
 
     log_request('start', event.user)
 
-    if @message_to_item.any? # reactions?
-      send(event, 'Detecting a previously running auction. Stopping it now.')
-      @message_to_item.clear
-      @message_to_reactions.clear
-    end
-
-    time = Time.new
-    date_string = time.strftime('%m/%d/%Y')
-    send(event, ":tada: Starting a new auction for #{date_string}, please wait a moment! :tada:")
-
-    AUCTION_ITEMS.each do |name, quantity|
-      message = format_auction_item(name, quantity, quantity, [])
-      e = send(event, message)
-      add_reactions(e.message)
-      @message_to_item[e.message] = name
-      @message_to_reactions[e.message] = {}
-    end
-
-    send(event,
-         'To claim something, react to its message with the quantity you want. For example, :two: means two of that item.
-Note: I am rate limited, so changes may take a minute to show up.
-:tada: The auction is ready! :tada:')
-
-    nil
+    @auction = Auction.new(@bot, event.channel.id)
+    @auction.start
   end
 
   def stop(event)
@@ -167,10 +67,7 @@ Note: I am rate limited, so changes may take a minute to show up.
 
     send(event, 'Stopping the auction!')
 
-    @message_to_item.clear
-    @message_to_reactions.clear
-
-    nil
+    @auction = nil
   end
 
   def do_exit(event)
